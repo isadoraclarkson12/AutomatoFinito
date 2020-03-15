1edf826a7593b777aaf43167602f5b901fea9c64
Isadora Faria <isadora_conectada_acabo@hotmail.com>
2020-03-15 19:24:55 -0300

    Versão final

--- src/automatofinito/AlternativaE.java
+++ src/automatofinito/AlternativaE.java
@@ -10,10 +10,10 @@ package automatofinito;
  * @author PURGISA
  */
 public class AlternativaE {
-       // E1 = {a, b}, E2 = {a, b, c, ... ,z} e E3 = {0, 1, 2, ... , 9}
+        // E1 = {a, b}, E2 = {a, b, c, ... ,z} e E3 = {0, 1, 2, ... , 9}
     // L5 = { w e E1* | w não possui ocorrência da cadeia baba }
     public static boolean testarSentenca(String s) {
-        if (s.length() > 4) {
+        if (s.length() < 4) {
             return true;
         } else {
             return teste(s, 0, 0);
@@ -30,7 +30,8 @@ public class AlternativaE {
         if (s.length() - i >= 4 && s.charAt(i) == 'b' && s.charAt(i + 1) == 'a'
                 && s.charAt(i + 2) == 'b' && s.charAt(i + 3) == 'a') {
             return teste(s, i + 1, n + 1);
+        } else {
+            return teste(s, i + 1, n);
         }
-        else return teste(s,i+1,n);
     }
 }
